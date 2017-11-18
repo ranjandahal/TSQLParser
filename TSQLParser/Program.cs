d4759f943ea06386abee2fbbefb8b643ff1e4c87
Ranjan Dahal <ranjan.dahal@yahoo.com>
2017-11-17 19:42:57 -0800

    Initial Commit

---
+++ TSQLParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.SqlServer.
+namespace TSQLParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
